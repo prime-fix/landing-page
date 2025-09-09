6c374d37ab050a1f5e666941d22d2b9e969314c8
Cesar- <legende.e@hotmail.com>
2025-09-09 15:31:54 -0500

    Enhance typography in Header component and global styles
    - Updated font weight for navigation links in Header.astro to 'bold' for improved emphasis.
    - Added new font variables for regular, semi-bold, bold, extra-bold, and italic styles in global.css.
    - Set default font family and color in global styles for better consistency across the application.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
